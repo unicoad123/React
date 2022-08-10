4866bf8a5391ec34ec44e76bc90af37ebec90bd3
unicoad123 <41484199+unicoad123@users.noreply.github.com>
2022-08-10 10:21:20 +0530

    login and authentication

--- src/App.js
+++ src/App.js
@@ -1,82 +1,25 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import MoviesList from './components/MoviesList';
-import AddMovie from './components/AddMovie';
-import './App.css';
+import Layout from './components/Layout/Layout';
+import UserProfile from './components/Profile/UserProfile';
+import AuthPage from './pages/AuthPage';
+import HomePage from './pages/HomePage';
 
 function App() {
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  const fetchMoviesHandler = useCallback(async () => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch('https://react-http-f7dbb-default-rtdb.firebaseio.com/movies.json');
-      if (!response.ok) {
-        throw new Error('Something went wrong!');
-      }
-
-      const data = await response.json();
-
-      const loadMovies=[];
-      for(const key in data)
-      {
-        loadMovies.push({
-          id:key,
-          title:data[key].title,
-          openingText:data[key].openingText,
-          releaseDate:data[key].releaseDate
-        })
-      }
-      setMovies(loadMovies);
-    } catch (error) {
-      setError(error.message);
-    }
-    setIsLoading(false);
-  }, []);
-
-  useEffect(() => {
-    fetchMoviesHandler();
-  }, [fetchMoviesHandler]);
-
-  async function addMovieHandler(movie) {
-    const response=await fetch('https://react-http-f7dbb-default-rtdb.firebaseio.com/movies.json',{
-      method:'POST',
-      body:JSON.stringify(movie),
-      headers:{
-        'Content-Type':'application/json'
-      }
-    });
-    const data=await response.json();
-    console.log(data);
-  }
-
-  let content = <p>Found no movies.</p>;
-
-  if (movies.length > 0) {
-    content = <MoviesList movies={movies} />;
-  }
-
-  if (error) {
-    content = <p>{error}</p>;
-  }
-
-  if (isLoading) {
-    content = <p>Loading...</p>;
-  }
-
   return (
-    <React.Fragment>
-      <section>
-        <AddMovie onAddMovie={addMovieHandler} />
-      </section>
-      <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-      </section>
-      <section>{content}</section>
-    </React.Fragment>
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <HomePage />
+        </Route>
+        <Route path='/auth'>
+          <AuthPage />
+        </Route>
+        <Route path='/profile'>
+          <UserProfile />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
